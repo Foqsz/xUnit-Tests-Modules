e001d62c5cc5fefb2cf4491b6c51ea3fcf588d8b
Victor Vinicius <victormafiavinicius@gmail.com>
2024-04-22 11:45:15 -0300

    Método criado e cenário pronto para ser iniciado

--- ModuloTestesDIO/Calculadora/Program.cs
+++ ModuloTestesDIO/Calculadora/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ModuloTestesDIO.Calculadora.Services;
 
-namespace ModuloTestesDIO.Calculadora
-{
-    internal class Program
-    {
-    }
-}
+CalculadoraImp c = new CalculadoraImp();
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
